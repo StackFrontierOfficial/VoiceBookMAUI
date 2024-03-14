51ce699e26b223747e72159ec2164bcf8872b75a
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:22:34 +0530

    Initial Commit

---
+++ VoiceBookMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VoiceBookMAUI
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ VoiceBookMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VoiceBookMAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
